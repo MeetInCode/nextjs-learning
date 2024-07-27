a372e7a3daaa4aa859904b0a73c7675a950212a9
meet mehta <meets.studentlife@gmail.com>
2024-07-28 01:11:22 +0530

    initial setup

---
+++ next-app/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function Home(){
+return (
+   <section className="py-10 px-10">
+    <div className="container">
+      <h1 className="text-3xl font-bold">hello world next is next</h1>
+    </div>
+   </section>
+)
+}
